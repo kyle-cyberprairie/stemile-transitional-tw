63fbc5e55331c9e316c4e3149efcca7f747a280e
Kyle <kyle@cyberprairie.ca>
2026-05-09 17:35:29 -0500

    Fix icon width

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -10,10 +10,10 @@
             document.querySelector('#{{ $id }}-minus-icon').classList.toggle('hidden')
             " class="accordion-header flex justify-between w-full text-left py-4 px-6 bg-gray-200 rounded-t focus:outline-none">
             {{ $title }}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="{{ $id }}-plus-icon" class="size-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="{{ $id }}-plus-icon" class="min-w-6 size-6 ml-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="{{ $id }}-minus-icon" class="size-6 hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="{{ $id }}-minus-icon" class="min-w-6 size-6 ml-2 hidden">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
 
